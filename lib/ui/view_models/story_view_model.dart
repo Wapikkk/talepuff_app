2b6a513ba68a063cbc723c0d1f9cac23b1000a18
Waffiq Muchlishin <waffiqw999@gmail.com>
2026-09-06 15:56:14 +0700

    feat: add audioplayers package, integrate ai services to UI enstotem

--- lib/ui/view_models/story_view_model.dart
+++ lib/ui/view_models/story_view_model.dart
@@ -1,21 +1,63 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 class StoryViewModel extends ChangeNotifier {
   bool _isLoading = false;
   bool get isLoading => _isLoading;
 
-  String _storyText = "";
+  String _storyText = "Choose category and sentiment to start the story!";
   String get storyText => _storyText;
 
-  Future<void> generateStory(String category, String sentiment) async {
+  final AudioPlayer _audioPlayer = AudioPlayer();
+  bool _isPlaying = false;
+  bool get isPlaying => _isPlaying;
+
+  Future<void> generateAndPlayStory(String category, String sentiment) async {
     _isLoading = true;
     notifyListeners();
 
-    // TODO: Input logic HTTP Request to FastAPI and Audio Player in here
-    await Future.delayed(const Duration(seconds: 2));
-    _storyText = "Stories about $category with a $sentiment vibe will appear here.";
+    try {
+      final baseUrl = dotenv.env['API_BASE_URL'] ?? "";
+      final url = Uri.parse('$baseUrl/generate-story');
 
-    _isLoading = false;
+      final response = await http.post(
+        url,
+        headers: {'Content-type': 'application/json'},
+        body: jsonEncode({
+          "category": category,
+          "sentiment": sentiment,
+        }),
+      );
+
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body);
+        _storyText = data['story'];
+
+        final audioUrl = data ['audio_url'];
+        await _audioPlayer.play(UrlSource(audioUrl));
+        _isPlaying = true;
+      } else {
+        _storyText = "Failed to create a story on the AI server";
+      }
+    } catch (e) {
+      _storyText = "Ensure the FastAPI server is running. Error: $e";
+    } finally {
+      _isLoading = true;
+      notifyListeners();
+    }
+  }
+
+  void togglePlayPause() async {
+    if (_isPlaying) {
+      await _audioPlayer.pause();
+    } else {
+      await _audioPlayer.pause();
+    }
+
+    _isPlaying = !_isPlaying;
     notifyListeners();
   }
 }
